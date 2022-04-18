b3a86070beb2dc0706c61b01b093c03f64e612da
Wiktor Jackowiak <jackowiak.g.wiktor@gmail.com>
2022-04-18 17:13:16 +0200

    update2sekcji

--- js/script.js
+++ js/script.js
@@ -55,54 +55,20 @@ window.addEventListener('load', () => {
   // }, 2000);
 });
 
+//popup {
+const openPopUp = document.querySelector('.btnCert');
+const closePopUp = document.querySelector('.btnCloseCert');
+const modal = document.querySelector('.modal');
 
-//popup 
+openPopUp.addEventListener('click', () => {
+  modal.classList.add('active');
+})
 
-$(".modal").each(function () {
-  $(this).wrap('<div class="overlay"></div>')
-});
+closePopUp.addEventListener('click', () => {
+  modal.classList.remove('active');
+})
 
-$(".open-modal").on('click', function (e) {
-  e.preventDefault();
-  e.stopImmediatePropagation;
 
-  var $this = $(this),
-    modal = $($this).data("modal");
-
-  $(modal).parents(".overlay").addClass("open");
-  setTimeout(function () {
-    $(modal).addClass("open");
-  }, 350);
-
-  $(document).on('click', function (e) {
-    var target = $(e.target);
-
-    if ($(target).hasClass("overlay")) {
-      $(target).find(".modal").each(function () {
-        $(this).removeClass("open");
-      });
-      setTimeout(function () {
-        $(target).removeClass("open");
-      }, 350);
-    }
-
-  });
-
-});
-
-$(".close-modal").on('click', function (e) {
-  e.preventDefault();
-  e.stopImmediatePropagation;
-
-  var $this = $(this),
-    modal = $($this).data("modal");
-
-  $(modal).removeClass("open");
-  setTimeout(function () {
-    $(modal).parents(".overlay").removeClass("open");
-  }, 350);
-
-});
 
 
 //social
@@ -130,11 +96,11 @@ const iks = document.querySelector('.iks')
 
 const showPopNav = function (e) {
   e.preventDefault()
-  wskShowed.classList.toggle('showed')
+  wskShowed.classList.add('showed')
 
 }
 
 wskazowki.addEventListener('click', showPopNav)
 iks.addEventListener('click', () => {
-  wskShowed.classList.toggle('showed')
+  wskShowed.classList.remove('showed')
 })
